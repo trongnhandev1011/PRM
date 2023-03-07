9ac6e5963f701edba0033213182b4de4f091a79a
trongnhandev1011 <nhantrongho2002@gmail.com>
2023-03-07 08:49:07 +0700

    feat: fix image url

--- app/src/main/java/com/example/test3/database/DatabaseOpener.java
+++ app/src/main/java/com/example/test3/database/DatabaseOpener.java
@@ -22,8 +22,8 @@ public class DatabaseOpener extends SQLiteOpenHelper {
         db.execSQL("INSERT INTO game VALUES('G1', 'Pokemon Scarlet', 'This is my dream game', 'Pokemon', 99, 10, 'https://vn-test-11.slatic.net/p/e95dc95dd19b2a76218bc198d76a1d1c.jpg')");
         db.execSQL("INSERT INTO game VALUES('G2', 'Pokemon Red', 'OMG This game is the best', 'Pokemon', 100, 10, 'https://m.media-amazon.com/images/I/71aow5iRsfL.jpg')");
         db.execSQL("INSERT INTO game VALUES('G3', 'Pokemon XYZ', 'No this game is better', 'Pokemon', 50, 20, 'https://upload.wikimedia.org/wikipedia/vi/d/d1/Pok%C3%A9mon_movie_2016.jpg')");
-        db.execSQL("INSERT INTO game VALUES('G4', 'Pokemon Sword', 'Shut up, this game is 99$', 'Pokemon', 30, 20, 'http://product.hstatic.net/1000231532/product/pokemon_sword_nintendo_switch_fb43376de31149ffaf605ceeedf248c4_grande.jpg')");
-        db.execSQL("INSERT INTO game VALUES('G5', 'Pokemon Shield', 'I choose this game and buy it!', 'Pokemon', 99, 15, 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.walmart.com%2Fip%2FPokemon-Shield-Nintendo-Switch-Physical-Edition-110457%2F919694071&psig=AOvVaw2aRNgSC3eSizSWTjywoPrg&ust=1678175425541000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCNC5guPoxv0CFQAAAAAdAAAAABAD')");
+        db.execSQL("INSERT INTO game VALUES('G4', 'Pokemon Sword', 'Shut up, this game is 99$', 'Pokemon', 30, 20, 'https://vn-test-11.slatic.net/p/ce0aed550cacddceb419ff486eccb66a.jpg')");
+        db.execSQL("INSERT INTO game VALUES('G5', 'Pokemon Shield', 'I choose this game and buy it!', 'Pokemon', 99, 15, 'https://i5.walmartimages.com/asr/b6436620-88f7-4de1-b83f-ab63a04d31c7_1.84fa4280b3c00768f867997304957768.jpeg')");
     }
 
     @Override
